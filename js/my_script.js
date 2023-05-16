255b790571b1f82a3884c9e4770134c7c3c46f80
Patrickhell <patrickhell19@yahoo.fr>
2023-05-16 16:45:32 +0200

    bonus 1

--- js/my_script.js
+++ js/my_script.js
@@ -1,3 +1,6 @@
+
+
+
 for(let x = 1 ; x <= 100 ; x++) {
 
     if( x % 3 == 0 &&  x % 5 == 0 ) {
@@ -5,10 +8,48 @@ for(let x = 1 ; x <= 100 ; x++) {
 
     }else if( x % 5 == 0) {
         console.log("BUZZ");
-        
+
     }else if( x % 3 == 0) {
         console.log("FIZZ");
     
     }else { console.log(x);
     }
+    
 }
+
+const ulElement = document.querySelector("ul");
+
+for(let x = 1 ; x <= 100 ; x++) {
+
+    if( x % 3 == 0 &&  x % 5 == 0 ) {
+    const liElement = document.createElement("li");
+    liElement. innerHTML = "Fizz-Buzz" ;
+     ulElement.appendChild(liElement);
+
+
+    }else if( x % 5 == 0) {
+        const liElement = document.createElement("li");
+        liElement. innerHTML = "Buzz" ;
+         ulElement.appendChild(liElement);
+
+    }else if( x % 3 == 0) {
+        const liElement = document.createElement("li");
+        liElement. innerHTML = "Fizz" ;
+         ulElement.appendChild(liElement);
+    
+    }else { 
+        const liElement = document.createElement("li");
+        liElement. innerHTML = x ;
+         ulElement.appendChild(liElement);
+
+    }
+    
+}
+
+     
+
+     
+
+
+
+     
