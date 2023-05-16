1dfbf458ebec91e76f1fb9755b462fb3c0d65d6d
Patrickhell <patrickhell19@yahoo.fr>
2023-05-16 17:04:58 +0200

    ultilizzo della proprietà className invece di classList.add per aggiungere la classe "colore ai diversi elementi.

--- js/my_script.js
+++ js/my_script.js
@@ -25,26 +25,26 @@ for(let x = 1 ; x <= 100 ; x++) {
     const liElement = document.createElement("li");
     liElement. innerHTML = "Fizz-Buzz" ;
      ulElement.appendChild(liElement);
-     liElement.classList.add ("blueviolet");
+     liElement.className = "blueviolet";
 
 
     }else if( x % 5 == 0) {
         const liElement = document.createElement("li");
         liElement. innerHTML = "Buzz" ;
          ulElement.appendChild(liElement);
-         liElement.classList.add ("cyan");
+         liElement.className = "cyan";
 
     }else if( x % 3 == 0) {
         const liElement = document.createElement("li");
         liElement. innerHTML = "Fizz" ;
          ulElement.appendChild(liElement);
-         liElement.classList.add ("red");
+         liElement.className = "red";
     
     }else { 
         const liElement = document.createElement("li");
         liElement. innerHTML = x ;
          ulElement.appendChild(liElement);
-         liElement.classList.add ("yellow");
+         liElement.className = "yellow";
 
     }
     
